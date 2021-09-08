7a5295a4aa6d5df1d5739975c5c1154bf7eb2d33
Duong Tung <19522494@gm.uit.edu.vn>
2021-09-08 10:31:54 +0700

    storing data

--- lib/helpers/db_helper.dart
+++ lib/helpers/db_helper.dart
@@ -3,9 +3,9 @@ import 'package:path/path.dart' as path;
 import 'package:sqflite/sqflite.dart';
 
 class DBHelper {
-  static Future<void> insert(String table, Map<String, Object> data) async {
+  static Future<Database> database() async {
     final dbPath = await sql.getDatabasesPath();
-    final sqlDb = await sql.openDatabase(
+    return sql.openDatabase(
       path.join(dbPath, 'places.db'),
       onCreate: (db, version) {
         return db.execute(
@@ -13,10 +13,19 @@ class DBHelper {
       },
       version: 1,
     );
-    await sqlDb.insert(
+  }
+
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    final db = await DBHelper.database();
+    db.insert(
       table,
       data,
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
+  }
+
+  static Future<List<Map<String, dynamic>>> getData(String table) async {
+    final db = await DBHelper.database();
+    return db.query(table);
   }
 }
